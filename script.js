1b79e1816681a43d641d2df36980984000f0187e
Rafi Mufadhal Difany <rafimdifany@gmail.com>
2021-02-28 13:04:50 +0700

    Using DOM Js, and change Graduation countdown concept into Birthday countdown Concept

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-const days = document.querySelector("#days");
-const hours = document.querySelector("#hours");
-const mins = document.querySelector("#mins");
-const hours = document.querySelector("#hours");
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
 
 //Target Countdown
-const graduation = "1 April 2021";
+const birthday = "8 May 2021";
 
 //Countdown FUnction
 function countdown() {
-  const graduationDate = new Date(graduation);
+  const birthdayDate = new Date(birthday);
   const currentDate = new Date();
 
-  const totalSeconds = (graduationDate - currentDate) / 1000;
+  const totalSeconds = (birthdayDate - currentDate) / 1000;
   const days = Math.floor(totalSeconds / 3600 / 24);
   const hours = Math.floor(totalSeconds / 3600) % 24;
-  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const mins = Math.floor(totalSeconds / 60) % 60;
   const seconds = Math.floor(totalSeconds % 60);
 
-  console.log(days, hours, minutes, seconds);
+  // console.log(days, hours, mins, seconds)
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minsEl.innerHTML = formatTime(mins);
+  secondsEl.innerHTML = formatTime(seconds);
 }
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
 //initial call
 countdown();
 
